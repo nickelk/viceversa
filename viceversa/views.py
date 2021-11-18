df268cf93523e89998fa1f75f92698544343eecd
nickelk <nick_smith@ukr.net>
2021-11-18 10:35:19 +0200

    reversed words feature added

--- viceversa/views.py
+++ viceversa/views.py
@@ -9,11 +9,12 @@ def reverse(request):
     user_text = request.GET['usertext']
     words = user_text.split()
     number_of_words = len(words)
-    reversed_words = words[-1]
-    print(reversed_words)
+    words.reverse()
+    reversed_words = ' '.join(words)
     reversed_text = user_text[::-1]
     return render(request,
                   'reverse.html',
-                  {'usertext':user_text,
-                  'reversed_text':reversed_text,
-                   'number_of_words': number_of_words})
+                  {'usertext': user_text,
+                   'reversed_text': reversed_text,
+                   'number_of_words': number_of_words,
+                   'reversed_words': reversed_words})
