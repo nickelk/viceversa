250cae80f8cfb9330984b73be553cf3e846582c7
nickelk <nick_smith@ukr.net>
2021-11-18 10:20:21 +0200

    counter feature added

--- viceversa/views.py
+++ viceversa/views.py
@@ -7,8 +7,13 @@ def home(request):
 
 def reverse(request):
     user_text = request.GET['usertext']
+    words = user_text.split()
+    number_of_words = len(words)
+    reversed_words = words[-1]
+    print(reversed_words)
     reversed_text = user_text[::-1]
     return render(request,
                   'reverse.html',
                   {'usertext':user_text,
-                  'reversed_text':reversed_text})
+                  'reversed_text':reversed_text,
+                   'number_of_words': number_of_words})
